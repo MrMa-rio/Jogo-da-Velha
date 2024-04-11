656d459ebf1059a693f2629aa805ad0d24a9a02c
Mario Alberto <112214759+MrMa-rio@users.noreply.github.com>
2024-04-11 17:41:07 -0300

    feat: adicionando conceitos de logica iniciais

--- src/main/java/org/jogo_da_Velha/Main.java
+++ src/main/java/org/jogo_da_Velha/Main.java
@@ -1,18 +1,62 @@
 package org.jogo_da_Velha;
 
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         String[][] tabuleiroDaVelha = new String[3][3];
-        VerificaTabuleiro verificaTabuleiro = new VerificaTabuleiro(tabuleiroDaVelha);
+        Scanner scanner = new Scanner(System.in);
+        int counter = 0;
+        VerificaTabuleiro verificaTabuleiro = new VerificaTabuleiro();
         for(int i = 0; i < tabuleiroDaVelha.length; i++){
             for (int j = 0; j < tabuleiroDaVelha.length; j++){
-                tabuleiroDaVelha[i][j] = "Velha";
+                tabuleiroDaVelha[i][j] = String.valueOf (counter++);
             }
         }
-        while(verificaTabuleiro.temVelha()){
+        do{
 
-        }
+            verificaTabuleiro.exibeTabuleiro(tabuleiroDaVelha);
+            System.out.println("Escolha uma posição para o 'X': ");
+            String playerX = scanner.nextLine();
+            for (String[] a : tabuleiroDaVelha) {
+                for (int j = 0; j < tabuleiroDaVelha.length; j++) {
+                    if (Objects.equals((playerX), a[j])) {
+                        a[j] = "X";
+                        break;
+                    }
+                }
+            }
+            verificaTabuleiro.exibeTabuleiro(tabuleiroDaVelha);
+
+            System.out.println("Escolha uma posição para o 'Y': ");
+            String playerY = scanner.nextLine();
+            for (String[] a : tabuleiroDaVelha) {
+                for (int j = 0; j < tabuleiroDaVelha.length; j++) {
+                    if (Objects.equals((playerY), a[j])) {
+                        a[j] = "Y";
+                        break;
+                    }
+                }
+            }
+            verificaTabuleiro.exibeTabuleiro(tabuleiroDaVelha);
+
+//            tabuleiroDaVelha[1][1] = "T";
+//            tabuleiroDaVelha[2][2] = "T";
+//
+//            tabuleiroDaVelha[0][1] = "O";
+//            tabuleiroDaVelha[0][2] = "0";
+//            tabuleiroDaVelha[1][2] = "0";
+//
+//            tabuleiroDaVelha[1][0] = "F";
+//            tabuleiroDaVelha[2][1] = "F";
+//            tabuleiroDaVelha[2][0] = "F";
+
+            //verificaTabuleiro.exibeTabuleiroVazio(tabuleiroDaVelha);
+
+
+        }while (verificaTabuleiro.temVelha(tabuleiroDaVelha));
 
     }
 
--- src/main/java/org/jogo_da_Velha/VerificaTabuleiro.java
+++ src/main/java/org/jogo_da_Velha/VerificaTabuleiro.java
@@ -3,12 +3,12 @@ package org.jogo_da_Velha;
 import java.util.Objects;
 
 public class VerificaTabuleiro{
-    String[][] tabuleiro;
+    //String[][] tabuleiro;
 
-    VerificaTabuleiro(String[][] tabuleiro){
-        this.tabuleiro = tabuleiro;
-    }
-    boolean naVertical(String player){
+    //VerificaTabuleiro(String[][] tabuleiro){
+//        this.tabuleiro = tabuleiro;
+//    }
+    boolean naVertical(String player, String[][] tabuleiro){
         for(int i = 0; i <= 2; i++){
             if(Objects.equals(player, tabuleiro[i][0])){
                 if(Objects.equals(player, tabuleiro[i][1])){
@@ -18,7 +18,7 @@ public class VerificaTabuleiro{
         }
         return false;
     }
-    boolean naHorizontal(String player){
+    boolean naHorizontal(String player, String[][] tabuleiro){
         for(int i = 0; i <= 2; i++){
             if(Objects.equals(player, tabuleiro[0][i])){
                 if(Objects.equals(player, tabuleiro[1][i])){
@@ -28,7 +28,7 @@ public class VerificaTabuleiro{
         }
         return false;
     }
-    boolean naSemiCruzEsqDir(String player){
+    boolean naSemiCruzEsqDir(String player, String[][] tabuleiro){
         if(Objects.equals(player, tabuleiro[0][0])){
             if(Objects.equals(player, tabuleiro[1][1])){
                 return Objects.equals(player, tabuleiro[2][2]);
@@ -36,7 +36,7 @@ public class VerificaTabuleiro{
         }
         return false;
     }
-    boolean naSemiCruzDirEsq(String player){
+    boolean naSemiCruzDirEsq(String player, String[][] tabuleiro){
         if(Objects.equals(player, tabuleiro[0][2])){
             if(Objects.equals(player, tabuleiro[1][1])){
                 return Objects.equals(player, tabuleiro[2][0]);
@@ -44,10 +44,11 @@ public class VerificaTabuleiro{
         }
         return false;
     }
-    boolean temVelha(){
+    boolean temVelha(String[][] tabuleiro){
+        int counter = 0;
         for (String[] strings : tabuleiro) {
             for (int j = 0; j < tabuleiro.length; j++) {
-                if (Objects.equals("Velha", strings[j])) {
+                if (Objects.equals(String.valueOf(counter++), strings[j])) {
                     return true;
                 }
             }
@@ -55,4 +56,28 @@ public class VerificaTabuleiro{
         return false;
     }
 
+    public void exibeTabuleiroVazio(String[][] tabuleiro) {
+        for (int i = 0; i < tabuleiro.length; i++){
+           if(i == 0){
+               System.out.printf(String.format("""
+                  %s  |  %s  |  %s
+
+                  """, i,i+1, i+2 ));
+           }
+           else{
+                System.out.printf(String.format("""
+                  %s  |  %s  |  %s
+
+                  """, i+2,i+3, i+4 ));
+            }
+        }
+    }
+    public void exibeTabuleiro(String[][] tabuleiro) {
+        for (String[] strings : tabuleiro) {
+            System.out.printf(String.format("""
+                    %s  |  %s  |  %s
+
+                    """, strings[0], strings[1], strings[2]));
+        }
+    }
 }
